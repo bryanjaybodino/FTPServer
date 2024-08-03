51cb1a38832a45dcb60876ce3cd472f6e937a597
Bryanjay Bodino <bryanjaybodino@gmail.com>
2024-08-03 16:41:37 +0800

    Add project files.

---
+++ FTPServer/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 2022;
+            string directory = "C://";
+
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties properties = ni.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ip in properties.UnicastAddresses)
+                    {
+                        Console.WriteLine("ftp://"+ip.Address.ToString()+":"+port);
+                    }
+                }
+            }
+
+       
+            Commands commands = new Commands(directory, port);
+            await commands.StartAsync();
+        }
+    }
+}
